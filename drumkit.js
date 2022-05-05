b6803c0d8e88673a0f367fa44af6ab13937ad79b
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-05-05 22:13:06 +0530

    Add files via upload

--- drumkit.js
+++ drumkit.js
@@ -1,3 +1,16 @@
+var numberOfButtons = document.querySelectorAll(".Button").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+document.querySelectorAll(".Button")[i]
+	.addEventListener("click", function() {
+	var buttonStyle = this.innerHTML;
+	sound(buttonStyle.toLowerCase());
+	animation(buttonStyle.toLowerCase());
+});
+}
+
+
+
 document.addEventListener("keypress",function(event){
   sound(event.key);
   animation(event.key);
@@ -12,22 +25,22 @@ function sound(letter)
       var sound=new Audio('A.mp3');
       sound.play();
       break;
-    case 'b':
+    case 's':
       var sound=new Audio('B.mp3');
       sound.play();
       break;
-    case 'c':
+    case 'd':
       var sound=new Audio('C.mp3');
       sound.play();
       break;
-    case 'd':
+    case 'f':
       var sound=new Audio('D.mp3');
       sound.play();
       break;
-    case 'e':
+    case 'g':
       var sound=new Audio('E.mp3');
       sound.play();
-    case 'f':
+    case 'h':
       var sound=new Audio('F.mp3');
       sound.play();
       break;
@@ -35,6 +48,7 @@ function sound(letter)
         console.log(letter);
   }
 }
+
 function animation(currentKey) {
 var activeButton = document.querySelector("." + currentKey.toUpperCase());
 
